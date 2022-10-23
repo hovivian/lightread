760128c99fa5d2da7f30fa5d44697e9c33d89e08
Vivian Ho <hovivian.wy@gmail.com>
2022-10-24 01:25:31 +0800

    updated libray

--- src/controllers/api/lightread/library.js
+++ src/controllers/api/lightread/library.js
@@ -1,9 +1,23 @@
 import prisma from '../../_helpers/prisma.js'
 import handleErrors from '../../_helpers/handle-errors.js'
 
+// validate
+//
+
 const controllersPagesLightReadLibrary = async () => {
   try {
-    //empty for now
+    const readingList = await prisma.readingList.update({
+      where: { userId },
+      data: {
+        books: {
+          connectOrCreate: {
+            where: { id: body.id },
+            create: { id: body.id, title: body.title, author: body.author },
+          }
+        }
+      }
+    })
+    res.json(readingList)
   } catch (err) {
     return handleErrors(res, err);
   }
